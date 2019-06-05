3e0c34e1bb1367013ffefef8df4e36e54796934e
ChelyYi <197163391@qq.com>
2019-06-05 03:43:02 +0200

    Update segmentation part

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,28 +1,119 @@
 import keras
+import keras.backend as K
 import tensorflow as tf
 import numpy as np
 from keras.applications.vgg16 import VGG16
-from keras.layers import UpSampling2D,Conv2D
+from keras.layers import UpSampling2D,Conv2D,Dropout,ZeroPadding2D,\
+    BatchNormalization,Activation,concatenate,GlobalAveragePooling2D,Dense
 from keras.models import Model
 
 
-x_train = np.load('../data/seg/x_seg_train.npy')
-y_train = np.load("../data/seg/y_seg_train.npy")
-print(y_train.shape)
 image_size = 256
+class_num = 21
+learning_rate = 0.0001
+epochs = 50
+
+def fcn_32(image_feature):
+    fc6 = Conv2D(kernel_size=(7,7),filters=4096,name="fullConv1",padding='same',activation='relu')(image_feature)
+    fc6 = Dropout(0.5)(fc6)
+    fc7 = Conv2D(kernel_size=(1,1),filters=4096,name="fullConv2",padding='same',activation='relu')(fc6)
+    fc7 = Dropout(0.5)(fc7)
+    score = Conv2D(kernel_size=(1,1),filters=class_num,name='scoreConv',padding='same',kernel_initializer='he_normal')(fc7)
+    result = Conv2D(kernel_size=(16,16),filters=class_num,name="32Deconv",padding='same',use_bias=False)\
+        (UpSampling2D(size=(32,32),name='32Upsampling',interpolation='bilinear')(score))
+
+    return result
+
+def segnet_decoder_block(input,filter,kernel_size=(3,3),padding='same',upsample=False):
+    if upsample: # do upsample first
+        input = UpSampling2D((2,2),interpolation='bilinear')(input)
+
+    output = Conv2D(filters=filter, kernel_size=kernel_size,kernel_initializer='he_normal', padding=padding)(input)
+    output = BatchNormalization()(output)
+    output = Activation('relu')(output)
+
+    return output
+
+
+def segnet(image_feature):
+    blockl_deconv1 = segnet_decoder_block(image_feature, filter=512,kernel_size=(3,3),upsample=True)
+    block1_deconv2 = segnet_decoder_block(blockl_deconv1,filter=512)
+    block1_deconv3 = segnet_decoder_block(block1_deconv2, filter=512)
+
+    block2_deconv1 = segnet_decoder_block(block1_deconv3,filter=512,upsample=True)
+    block2_deconv2 = segnet_decoder_block(block2_deconv1,filter=512)
+    block2_deconv3 = segnet_decoder_block(block2_deconv2,filter=256)
+
+    block3_deconv1 = segnet_decoder_block(block2_deconv3,filter=256,upsample=True)
+    block3_deconv2 = segnet_decoder_block(block3_deconv1,filter=256)
+    block3_deconv3 = segnet_decoder_block(block3_deconv2,filter=128)
+
+    block4_deconv1 = segnet_decoder_block(block3_deconv3,filter=128,upsample=True)
+    block4_deconv2 = segnet_decoder_block(block4_deconv1,filter=64)
+
+    block5_deconv1 = segnet_decoder_block(block4_deconv2,filter=64,upsample=True)
+    result = BatchNormalization()(Conv2D(filters=class_num,kernel_size=(1,1),padding='same')(block5_deconv1))
+
+    return result
+
+def unet_upblock(filter,input,concat,concat_encoder):
+    up_conv = BatchNormalization()(Conv2D(filters=filter,kernel_size=(2,2),activation='relu',padding='same',
+                                          kernel_initializer='he_normal')(UpSampling2D(size=(2,2),interpolation='bilinear')(input)))
+    if concat:
+        merge = concatenate([concat_encoder,up_conv])
+    else:
+        merge = up_conv
+    conv1 = BatchNormalization()(Conv2D(filters=filter,kernel_size=(3,3),activation='relu',padding='same',
+                                        kernel_initializer='he_normal')(merge))
+    conv2 = BatchNormalization()(Conv2D(filters=filter, kernel_size=(3, 3), activation='relu', padding='same',
+                                        kernel_initializer='he_normal')(conv1))
+
+    return conv2
+
+
+def unet(image_feature,vgg):
+    conv1 = Conv2D(filters=1024,kernel_size=(3,3),activation='relu',padding='same',kernel_initializer='he_normal')(image_feature)
+    conv2 = Conv2D(filters=1024, kernel_size=(3,3), activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    decoder_input = Dropout(0.5)(conv2)
+
+    decoder1 = unet_upblock(filter=512,input=decoder_input,concat_encoder=vgg.get_layer('block5_conv3').output,concat=True)
+    decoder2 = unet_upblock(filter=256,input=decoder1,concat_encoder=vgg.get_layer('block4_conv3').output,concat=True)
+    decoder3 = unet_upblock(filter=128,input=decoder2,concat_encoder=vgg.get_layer('block3_conv2').output,concat=False)
+    decoder4 = unet_upblock(filter=64,input=decoder3,concat_encoder=vgg.get_layer('block2_conv2').output,concat=False)
+    decoder5 = unet_upblock(filter=64, input=decoder4, concat_encoder=vgg.get_layer('block1_conv2').output,concat=False)
+
+    result = Conv2D(filters=class_num,kernel_size=(1,1),activation='relu',padding='same',
+                    kernel_initializer='he_normal',name='seg_output')(decoder5)
+
+    return result
+
+
+# def cross_entropy_loss(y_pred,y_true):
+#     return tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_pred,logits=y_true)
+
+def softmax_sparse_crossentropy(y_true, y_pred):
+    y_pred = K.reshape(y_pred, (-1, K.int_shape(y_pred)[-1]))
+    log_softmax = tf.nn.log_softmax(y_pred)
+
+    y_true = K.one_hot(tf.to_int32(K.flatten(y_true)), K.int_shape(y_pred)[-1])
+    unpacked = tf.unstack(y_true, axis=-1)
+    y_true = tf.stack(unpacked[:], axis=-1)
+
+    cross_entropy = -K.sum(y_true * log_softmax, axis=1)
+    cross_entropy_mean = K.mean(cross_entropy)
+
+    return cross_entropy_mean
 
 vgg = VGG16(weights='imagenet',include_top = False,input_shape=(image_size,image_size,3))
 image_feature = vgg.output
-result = Conv2D(kernel_size=(3,3),filters=21,name="32Deconv",padding='same')(UpSampling2D(size=(32,32),name='32Upsampling')(image_feature))
+result = unet(image_feature,vgg)
 
-model = Model(vgg.input,result)
-
-def cross_entropy_loss(y_pred,y_true):
-    return tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_pred,logits=y_true)
-
+model = Model(vgg.input, result)
 model.summary()
-output_target = tf.placeholder(dtype="int32",shape=(None,256,256))
-model.compile(optimizer=keras.optimizers.Adam(lr=0.001),loss=cross_entropy_loss,target_tensors=[output_target])
-model.fit(x=x_train,y=y_train,batch_size=32,epochs=30,validation_split=0.1)
 
-model.save('..model/segmentation1.h5')
+model.compile(optimizer=keras.optimizers.Adam(lr=learning_rate),loss=softmax_sparse_crossentropy)#target_tensors=[output_target])
+
+x_train = np.load('../data/seg/x_seg_train.npy')
+y_train = np.load("../data/seg/y_seg_train.npy")
+model.compile(optimizer=keras.optimizers.Adam(lr=learning_rate,decay=learning_rate/epochs),loss=softmax_sparse_crossentropy)
+model.fit(x=x_train,y=y_train,batch_size=16,epochs=epochs,validation_split=0.1,shuffle=True)
--- src/segmentation_evaluate.py
+++ src/segmentation_evaluate.py
@@ -7,20 +7,27 @@ def mean_iu(predict,gt):
     :return: mean iu value
     """
     iu = 0
-    classes = np.unique(predict)# classes in predict result
-    class_num = classes.shape[0]
-    if class_num == 1: # only background
-        return 0.0
+    predict_classes = np.unique(predict)# classes in predict result
+    gt_classes = np.unique(gt)
+
+    if predict_classes.shape[0] >= gt_classes.shape[0]:
+        class_num = predict_classes.shape[0]
+        classes = predict_classes
+    else:
+        class_num = gt_classes.shape[0]
+        classes = gt_classes
+
+    # if class_num == 1: # only background
+    #     return 0.0
     for c in classes:
-        if c == 0: # skip background class
-            continue
+        # if c == 0: # skip background class
+        #     continue
         p_ii =element_wise_same(predict,gt,c)
         t_i = np.count_nonzero(gt == c)
         p_ij = np.count_nonzero(predict == c)
         if t_i != 0 or p_ij != 0:
             iu += p_ii/(t_i + p_ij - p_ii)
-    mean_iu = iu / (class_num-1)
-    print(mean_iu)
+    mean_iu = iu / class_num
     return mean_iu
 
 def element_wise_same(predict,gt,c):
